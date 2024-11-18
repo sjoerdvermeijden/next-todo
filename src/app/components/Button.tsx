df69e3ee070bb0c6ef5ee008c85c38f6fce9b162
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-11-18 14:59:15 +0100

    Adds functions to the button component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,12 +3,13 @@ import clsx from 'clsx'
 
 type Props = {
     children: React.ReactNode,
-    className?: string
+    className?: string,
+    buttonFunction?: () => void,
 }
 
-function Button({ children, className }: Props) {
+function Button({ children, className, buttonFunction }: Props) {
     return (
-        <div className={clsx('py-1 px-2 text-white bg-red-200 rounded hover:cursor-pointer', className)}>{children}</div>
+        <div className={clsx('py-1 px-2 text-white bg-red-200 rounded hover:cursor-pointer', className)} onClick={buttonFunction}>{children}</div>
     )
 }
 
--- src/app/components/Todos.tsx
+++ src/app/components/Todos.tsx
@@ -1,17 +1,31 @@
+'use client'
+
 import React from 'react'
 
 import Button from './Button'
+
+const deleteTodo = () => {
+    console.log('Delete')
+}
+
+const completeTodo = () => {
+    console.log('Complete')
+}
+
+const updateTodo = () => {
+    console.log('Update')
+}
 
 function Todos() {
     return (
         <ul className='flex flex-col gap-2'>
             <li className='flex'>
-                <div className='grow bg-white flex items-center px-2 rounded mr-2'>
+                <div className='grow bg-white flex items-center px-3 rounded mr-2'>
                     <span className=''>Todo</span>
                 </div>
-                <Button className="mr-1 bg-slate-800">Edit</Button>
-                <Button className="mr-1 bg-red-500">Completed</Button>
-                <Button className="bg-purple-500">Delete</Button>
+                <Button className="mr-1 bg-slate-800" buttonFunction={updateTodo}>Edit</Button>
+                <Button className="mr-1 bg-green-500" buttonFunction={completeTodo}>Completed</Button>
+                <Button className="bg-red-500" buttonFunction={deleteTodo}>Delete</Button>
             </li>
         </ul>
     )
